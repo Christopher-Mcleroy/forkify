26cf4856ec87d5ed4bf4b603ea0e5ba7c8390736
allenwebdev <62407430+allenwebdev@users.noreply.github.com>
2020-07-17 11:33:26 -0700

    added searchView

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,21 @@
 import Search from './model/Search';
 import { elements } from './view/base';
+import * as searchView from './view/searchView';
 
 const state = {};
 
 async function controlSearch() {
   //1 get input from view
-  const query = 'pizza';
+  const query = searchView.getInput();
   if (query) {
     //2 create new search
     state.search = new Search(query);
     //3 clear input from view
+    searchView.clearInput();
     //4. get recipes
     await state.search.getData();
     //5. display results
-    console.log(state.search.recipes);
+    searchView.renderRecipes(state.search.recipes);
   }
 }
 
--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -1,1 +1,32 @@
 import { elements } from './base';
+
+export const getInput = () => elements.searchInput.value;
+
+export const clearInput = () => (elements.searchInput.value = '');
+
+export const renderRecipes = (recipes) => {
+  elements.searchResults.innerHTML = '';
+  recipes.forEach((e) => displayRecipe(e));
+};
+// image_url: 'http://forkify-api.herokuapp.com/images/pestoa0e7.jpg';
+// publisher: 'The Pioneer Woman';
+// publisher_url: 'http://thepioneerwoman.com';
+// recipe_id: '47025';
+// social_rank: 100;
+// source_url: 'http://thepioneerwoman.com/cooking/2011/06/pasta-with-pesto-cream-sauce/';
+// title: 'Pasta with Pesto Cream Sauce';
+
+function displayRecipe(recipe) {
+  const html = `<li>
+                    <a class="results__link results__link--active" href="#${recipe.recipe_id}">
+                        <figure class="results__fig">
+                            <img src="${recipe.image_url}" alt="Test">
+                        </figure>
+                        <div class="results__data">
+                            <h4 class="results__name">${recipe.title}</h4>
+                            <p class="results__author">${recipe.publisher}</p>
+                        </div>
+                    </a>
+                </li>`;
+  elements.searchResults.insertAdjacentHTML('beforeend', html);
+}
