dafd66a93a8b9f4884d6e7d93fcf7b4e1a3935b1
Chris Mcleroy <allen.mcleroy@gmail.com>
2021-02-17 15:53:35 -0800

    removed comments from index.js

--- src/js/index.js
+++ src/js/index.js
@@ -12,34 +12,16 @@ import '../scss/main.scss';
 
 const state = {};
 
-// ************************************************************************************************** */
-// ******************************controller for search   ********************************************* */
-// ************************************************************************************************** */
-
 async function controlSearch(page) {
-  // ************************************************************************************************** */
-  // ******************************************testing    ********************************************* */
-  // ************************************************************************************************** */
-  // 1 get input from view
   const query = searchView.getInput();
-  // const query = 'pizza';
-  // ************************************************************************************************** */
-  // ******************************************testing    ********************************************* */
-  // ************************************************************************************************** */
   if (query) {
-    // 2 create new search
     state.search = new Search(query);
-    // 3 clear input from view
     searchView.clearInput();
-    // 4. render loader
     renderLoader(elements.searchRes);
-    // 5. get recipes
     await state.search
       .getData()
       .then(() => {
-        // 6. remove loader
         removeLoader();
-        // 7. display results
         searchView.renderRecipes(state.search.recipes, page);
       })
       .catch((err) => {
@@ -48,33 +30,19 @@ async function controlSearch(page) {
   }
 }
 
-// ************************************************************************************************** */
-// ******************************controller for items   ********************************************* */
-// ************************************************************************************************** */
-
 async function controlItem() {
-  // 1. get selected id
   const id = window.location.hash.replace('#', '');
   if (id) {
     searchView.clearSelected();
     searchView.highlightSelected(id);
-    // 2. save in state
     state.recipe = new Recipe(id);
-    // 3. render loader
     renderLoader(elements.recipe);
-    // 4. get item data
     await state.recipe.getItemData();
-    // parse ingredients
     state.recipe.parseIngredients();
-    // 5. remove loader
     removeLoader();
-    // calc time
     state.recipe.calcRecipeTime();
-    // calc servings
     state.recipe.calcServings();
-    // 6. render new item
     recipe.renderRecipe(state.recipe);
-    // if liked fills in like btn
     if (state.likes) {
       if (state.likes.isLiked(state.recipe.id) !== -1) {
         likesView.toggleLikeBtn();
@@ -82,10 +50,6 @@ async function controlItem() {
     }
   }
 }
-
-// ************************************************************************************************** */
-// ******************************controller for shopping cart   ************************************* */
-// ************************************************************************************************** */
 
 const controlCart = () => {
   if (!state.list) state.list = new ShoppingList();
@@ -95,44 +59,27 @@ const controlCart = () => {
   state.list.items.forEach((el) => shoppingCartView.renderShoppingCartItem(el));
 };
 
-// ************************************************************************************************** */
-// ******************************controller for like            ************************************* */
-// ************************************************************************************************** */
-
 const controlLike = (state) => {
   if (!state.likes) state.likes = new Likes();
   const { id } = state.recipe;
   if (state.likes.isLiked(id) === -1) {
     state.likes.addLike(state.recipe);
-    // toggle button
     likesView.toggleLikeBtn();
-    // add to ui
     likesView.addLike(state.likes.likes.find((el) => el.id === id));
   } else {
-    // remove like
     state.likes.removeLike(id);
-    // toggle button
     likesView.toggleLikeBtn();
-    // remove from ui
     likesView.removeLike(id);
   }
 };
 
-// ************************************************************************************************** */
-// ******************************event listeners***************************************************** */
-// ************************************************************************************************** */
-
 window.addEventListener('load', () => {
   const id = window.location.hash.replace('#', '');
-  // add new like
   state.likes = new Likes();
-  // get likes from storage
   state.likes.readStorage();
-  // render likes
   state.likes.likes.forEach((like) => likesView.addLike(like));
 });
 
-// event listener for  recipe
 elements.recipe.addEventListener('click', (e) => {
   if (e.target.matches('.btn-decrease, .btn-decrease *')) {
     if (state.recipe.servings > 1) {
@@ -149,58 +96,33 @@ elements.recipe.addEventListener('click', (e) => {
   }
 });
 
-// shopping cart remove item
 elements.shoppingList.addEventListener('click', (e) => {
-  // remove item if remove btn is clicked
   if (e.target.matches('.shopping__btn *')) {
-    // get id
     const id = e.target.closest('.shopping__item').dataset.itemid;
     state.list.removeItem(id);
     shoppingCartView.removeShoppingCartItem(id);
   }
 });
 
-// shopping cart change item amount
 elements.shoppingList.addEventListener('change', (e) => {
-  // get id
   const id = e.target.closest('.shopping__item').dataset.itemid;
-  // gets value and updates
   if (e.target.matches('[type="number"]')) {
     const value = parseFloat(e.target.value, 10);
     state.list.updateItem(id, value);
   }
 });
 
-// search submit event
 elements.searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  // render search results
   controlSearch();
 });
 
-// search page buttons
 elements.resultPages.addEventListener('click', (e) => {
   const btn = e.target.closest('.btn-inline');
   const page = parseInt(btn.getAttribute('data-page'), 10);
   searchView.renderRecipes(state.search.recipes, page);
 });
 
-// event listener to load recipe
 ['hashchange', 'load'].forEach((event) =>
   window.addEventListener(event, controlItem)
 );
-
-// ************************************************************************************************** */
-// ******************************************testing    ********************************************* */
-// ************************************************************************************************** */
-
-// window.addEventListener('load', (e) => {
-//   e.preventDefault();
-//   controlSearch();
-// });
-
-// window.s = state;
-
-// ************************************************************************************************** */
-// ******************************************testing    ********************************************* */
-// ************************************************************************************************** */
